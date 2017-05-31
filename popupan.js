d2e1c78baa019d9c08bdf81b46580fec7c064dc3
Vespalx8888 <vespalx8888@gmail.com>
2017-05-31 13:15:39 +0700

    Add files via upload

--- popupan.js
+++ popupan.js
@@ -2,7 +2,7 @@
 	var url_popup = 'http://tragoppiaggio.vn';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'http://tragoppiaggio.vn';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
